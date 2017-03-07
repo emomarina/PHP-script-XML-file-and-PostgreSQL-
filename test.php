84c6e28d42f460ad337280747d7ebd54d0aefeef
Emil Tenev <sunny7eood@abv.bg>
2017-03-07 03:48:52 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,28 @@
-
+<?xml version="1.0" encoding="UTF-8"?>
+<employees>
+    
+    <user id="1">
+        <name>Emili</name>
+        <job> nurse</job>
+        <personality>sister</personality>
+        <salary>$400</salary>
+		<level>high</level>
+        <email>nurse@abv.bg</email>
+    </user>
+    <user id="2">
+    <name>Stela</name>
+        <job> nurse</job>
+        <personality>sister</personality>
+        <salary>$330</salary>
+		<level>high</level>
+        <email>nurse@abv.bg</email>
+	</user>
+	<user id="3">
+    <name>Vera</name>
+        <job> nurse</job>
+        <personality>sister</personality>
+        <salary>$450</salary>
+		<level>high</level>
+        <email>nurse@abv.bg</email>
+	</user>
+    </employees>
